0926592a94d7f21ec880192127569835571c2803
Emmanuel Tapiwanashe Chingore <etc4@slurm1.hood.edu>
2023-10-26 20:37:26 -0400

    work done

--- Figure Script.R
+++ Figure Script.R
@@ -1,6 +1,9 @@
+#figure_plot
+
 ##### Command Line Practice ##################
 
 ####loading packages########
+
 library(dplyr)
 library(ggplot2)
 
@@ -10,7 +13,8 @@ dat <- tibble(x = rnorm(100), y = x + rnorm(100, sd = 0.2))
 
 #Plotting figure
 
-ggplot(dat, aes(x, y)) + 
+gg1 <- ggplot(dat, aes(x, y)) + 
   geom_point(alpha = 0.2) +
   geom_abline(slope = 1, intercept = 0)
 
+ggsave(gg1, filename = "figure_plot.png")
